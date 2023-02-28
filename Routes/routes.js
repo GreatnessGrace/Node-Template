d891cc0d830291d96b0f09fb4003d14d9fad7a7a
GreatnessGrace <125429418+GreatnessGrace@users.noreply.github.com>
2023-02-28 12:25:56 +0530

    Delete Routes directory

--- Routes/routes.js
+++
@@ -1,7 +0,0 @@
-// We can define all our routes here for improve code readability
-const express = require('express');
-const router = express.Router()
-const userCtrl= require('../controllers/controllers')
-
-router.get('/',userCtrl);
-module.exports = router
